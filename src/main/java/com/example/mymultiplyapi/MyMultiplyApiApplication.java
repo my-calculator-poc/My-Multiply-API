5f47bc11e4203d43c7d2c1a65825baf5a5167e6c
Pieter (P.H.C.) Vallen <Pieter.Vallen@ing.com>
2019-06-14 10:55:03 +0200

    Proper naming

--- src/main/java/com/example/mymultiplyapi/MyMultiplyApiApplication.java
+++ src/main/java/com/example/mymultiplyapi/MyMultiplyApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.MyMultiplyAPI;
+package com.example.mymultiplyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/mymultiplyapi/MyMultiplyApiApplicationTests.java
+++ src/test/java/com/example/mymultiplyapi/MyMultiplyApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.MyMultiplyAPI;
+package com.example.mymultiplyapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
